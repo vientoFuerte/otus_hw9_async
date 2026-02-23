a8631177157f15d28e1eba684186247d10415d22
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-02-23 17:50:56 +0300

    Update async.h
    Добавлен интерфейс для обработки команд.

--- async.h
+++ async.h
@@ -5,3 +5,20 @@
 #include <iostream>
 #include <fstream>
 
+
+namespace async {
+
+struct BulkContext {
+    std::size_t bulk_size;
+    std::vector<std::string> commands;
+
+    BulkContext(std::size_t size) : bulk_size(size) {}
+
+};
+
+
+BulkContext * connect(std::size_t bulk);
+void receive(BulkContext * ctx, const char* data, std::size_t size);
+void disconnect(BulkContext * ctx);
+
+} 
