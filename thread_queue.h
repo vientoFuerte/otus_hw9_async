2e1020fe0310b1aaa612e6dcf4ff9b5dd00dc4a3
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-03-06 23:31:36 +0300

    Add files via upload
    Добавлена реализация потокобезопасной очереди.

---
+++ thread_queue.h
@@ -0,0 +1,55 @@
+#pragma once
+
+
+#include <condition_variable>
+#include <string>
+#include <queue>
+#include <atomic>
+#include <mutex>
+
+/* Реализация потокобезопасной очереди. Операции добавления и вставки
+push() и pop() выполняются при залоченном мьютексе. */
+
+template<typename T>
+class thread_queue
+{
+	std::queue<T> queue_;           // формируемая очередь
+	std::mutex mutex_;              // мьютекс для защиты доступа к очереди
+	std::condition_variable cv_;    // условая переменная - поток будет ждать данные
+	bool done_ = false;             // флаг для завершения ожидания
+	public:
+	    //добавить элемент в очереди, предварительно ее заблокировав
+		void push(const T& value)
+		{
+			std::unique_lock<std::mutex> lock(mutex_);
+			queue_.push(value);
+			cv_.notify_one();
+		}
+		//достать элемент из очереди, предварительно ее заблокировав
+		bool pop(T& value)
+		{
+			std::unique_lock<std::mutex> lock(mutex_);
+			// ждем появления данных, пока очередь пустая - спим.
+			cv_.wait(lock, [] {return !queue_.empty() || done_; });
+			if(queue_.empty()) {return false;}
+			value = queue_.front();
+			queue_.pop(value);
+			return true;
+		}
+		void stop()
+		{
+			std::unique_lock<std::mutex> lock(mutex_);
+			done_ = true;
+			cv_.notify_all();
+		}
+		
+	
+};
+
+
+
+
+
+
+
+
